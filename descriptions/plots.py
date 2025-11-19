51fb9d2a56a7a7ef5158baa11ea7a514d9081c2f
Christian <christianfullerton@icloud.com>
2025-11-19 15:26:03 -0600

    fix: Add pass statement to plots.py to prevent it from causing errors

--- descriptions/plots.py
+++ descriptions/plots.py
@@ -23,6 +23,7 @@ def main(
             logger.info("Something happened for iteration 5.")
     logger.success("Plot generation complete.")
     # -----------------------------------------
+    pass
 
 
 if __name__ == "__main__":
